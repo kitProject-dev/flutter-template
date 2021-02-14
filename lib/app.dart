cf6b7dc1d0ceba7c8aa1785f361b6a1311491b4d
KoheiYamashita <k-yamashita@kitproject.info>
2021-02-14 12:02:33 +0900

    Create multi platform build

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
