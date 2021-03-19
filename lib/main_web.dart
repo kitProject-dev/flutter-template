9a3b7fcd357bb2b480a60be8e89f12eb04437c50
KoheiYamashita <k-yamashita@kitproject.info>
2021-03-19 02:07:00 +0000

    Add firebase web

--- lib/main_web.dart
+++ lib/main_web.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/app.dart';
 import 'package:flutter_template/repository/counter_repository.dart';
 import 'package:flutter_template/web/repository/counter_repository_web.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     ProviderScope(
       overrides: [
